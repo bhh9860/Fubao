59340c0e4a68d5a4d90c7f1726ed70d2e79b1953
bhh9860 <bhh9860@naver.com>
2023-10-05 13:08:37 +0900

    cloudtype mysql

--- server/connection.js
+++ server/connection.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
+  host: 'svc.sel5.cloudtype.app',
+  user: 'hyeon',
   password: '1234',
   database: 'fubao',
+  port: 32234,
 });
 
 module.exports = connection;
